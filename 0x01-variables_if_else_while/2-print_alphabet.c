42878ec494ab4028048173eb64f08373c08f3e27
Alex kimilu <alexmuia836@gmail.com>
2022-10-19 15:48:21 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,13 @@
 int main(void)
 {
 	char i;
-	
-	for (i = 'a'; i <= 'z'; i++)
+
+        for(i = 'a'; i <= 'z'; i++)
+
 	{
-		putchar(i);
+		putchar (i);
 	}
-	putchar('\n');
+
+	putchar ('\n');
 	return(0);
 }
