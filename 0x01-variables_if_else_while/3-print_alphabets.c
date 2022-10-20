f1877745aef8d228aa4e461f707ba9e653588194
Alex kimilu <alexmuia836@gmail.com>
2022-10-19 17:05:50 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
